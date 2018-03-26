71edd1bbc24f9a96e73b6e7562b531f98472d76f
mi2428 <mi2428782020@gmail.com>
2018-03-26 17:17:44 +0900

    Save content to local storage
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- logic.js
+++ logic.js
@@ -1,3 +1,23 @@
+$(document).ready(() => {
+  chrome.storage.sync.get(['title', 'content'], (result) => {
+    $("#title").val(result.title);
+    $("#content").val(result.content);
+  });
+});
+
 $('#content').on('change keyup keydown paste cut', function(){
   $(this).height(this.scrollHeight);
+});
+
+$(document).keydown((e) => {
+  if ((e.ctrlKey || e.metaKey) && e.which == 83) {
+    const notetitle = $("#title").val();
+    const notecontent = $("#content").val();
+    console.log(notecontent);
+    const storage = {'title': notetitle, 'content': notecontent};
+    e.preventDefault();
+    chrome.storage.sync.set(storage, () => {
+      console.log("Note saved!");
+    });
+  }
 });
